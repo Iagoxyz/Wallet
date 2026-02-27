22d0d934f9232d01767995e30f688e006a82d902
Iago Carvalho <iagocarvalhos10@gmail.com>
2026-02-27 16:24:22 -0300

    Project created

---
+++ src/test/java/tech/backend/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.backend.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
