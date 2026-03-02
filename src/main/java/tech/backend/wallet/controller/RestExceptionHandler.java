84b183c21f234f0b98e4ebd9c20edf068d25b6f0
Iago Carvalho <iagocarvalhos10@gmail.com>
2026-03-02 11:25:03 -0300

    Refine exception handling

--- src/main/java/tech/backend/wallet/controller/RestExceptionHandler.java
+++ src/main/java/tech/backend/wallet/controller/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package tech.backend.wallet.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.backend.wallet.exception.WalletException;
@@ -12,4 +14,23 @@ public class RestExceptionHandler {
     public ProblemDetail handlerWalletException(WalletException e) {
         return e.toProblemDetail();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        var fieldErros = e.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        pb.setTitle("Your request parameters didn't valitade.");
+        pb.setProperty("invalid-params", fieldErros);
+
+        return pb;
+    }
+
+
+    private record InvalidParam(String name, String reason) {}
 }
